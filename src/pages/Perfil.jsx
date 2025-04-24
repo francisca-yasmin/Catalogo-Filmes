be0b5109ff3e83ee19d73b7528f541d90e32f03b
Francisca Yasmin Freitas de Almeida <47915176870@>
2025-04-24 11:33:43 -0300

    tela de perfil finalizada

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -8,8 +8,8 @@ export function Perfil(){
     return(
          <>
 
-{/* bloco de perfil do usuario onde contem, icone de imagem
-seção de ultimos assistidos, favoritos e uma lista do usuario */}
+                {/* bloco de perfil do usuario onde contem, icone de imagem
+                    seção de ultimos assistidos, favoritos e uma lista do usuario */}
             <div className={estilos.container}>
             <div className={estilos.header}>
                 <img src={miranha} alt="Avatar" className={estilos.avatar} />
